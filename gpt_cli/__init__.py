a0b0fff1d9eb947b74a731c5548acde388115338
Sergio Mattei <sergiomattei@outlook.com>
2023-05-14 22:09:12 -0400

    [feat] Fix styling and add error for no token

--- gpt_cli/__init__.py
+++ gpt_cli/__init__.py
@@ -7,17 +7,26 @@ import openai
 import click
 
 console = Console()
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
 
 state = [{"role": "system", "content": "You are a helpful assistant."}]
 
 
 @click.command()
 @click.argument("text", required=False)
-def main(text):
+@click.option(
+    "--token",
+    default=lambda: os.environ.get("OPENAI_API_KEY", ""),
+    help="Your OpenAI API token",
+)
+def main(text, token):
+    if not token:
+        console.print(
+            "[bold red]No token provided.[/] Pass --token or set OPENAI_API_KEY in your environment."
+        )
+        exit(1)
+
     while True:
-        message = console.input("[black on cyan] > [/] ") if not text else text
+        message = console.input("[cyan]>[/] ") if not text else text
         if message == "q" or message == "exit":
             exit(0)
         state.append({"role": "user", "content": message})
